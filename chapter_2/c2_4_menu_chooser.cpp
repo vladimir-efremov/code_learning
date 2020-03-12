dd713d3fe37e83e111b9094b4fa42270f52b64f5
zetsubo <rinji.kurai@gmail.com>
2020-03-12 20:51:04 +0800

    Chapter 2 - Menu chooser

---
+++ chapter_2/c2_4_menu_chooser.cpp
@@ -0,0 +1,40 @@
+// Score Rater 3.0
+// Условия
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		
+	cout << "Difficulty levels:\n\n";
+	cout << "1 - Easy\n";
+	cout << "2 - Normal\n";
+	cout << "3 - Hard\n";
+	
+	int choice;
+	cout << "Choise: ";
+	cin >> choice;
+	
+	switch(choice)
+	{
+		case 1:
+			cout << "You picked Easy.\n";
+			break;
+			
+		case 2:
+			cout << "You picked Normal.\n";
+			break;
+			
+		case 3:
+			cout << "You picked Hard.\n";
+			break;
+			
+		default:
+			cout << "You made an illegal choice.\n";
+			break;
+	}
+		
+	return 0;
+}
