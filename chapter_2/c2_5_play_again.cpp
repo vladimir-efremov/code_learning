12685615111b0210984a4dcc9a51bfe680eb7d41
zetsubo <rinji.kurai@gmail.com>
2020-03-12 20:57:43 +0800

    Chapter 2 - Play again

---
+++ chapter_2/c2_5_play_again.cpp
@@ -0,0 +1,24 @@
+// Score Rater 3.0
+// Условия
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		
+	char again = 'y';
+	
+	while(again == 'y')
+	{
+		cout << "\n**Played an exciting game**";
+		cout << "\nDo yo want to play again? (y/n): ";
+	
+		cin >> again;
+	}
+	
+	cout << "\nOkay, bye.";	
+		
+	return 0;
+}
